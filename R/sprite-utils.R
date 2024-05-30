435ca4290e5c05091d52be239ed44636c2ed99b6
mike <mikefc@coolbutuseless.com>
2024-05-30 10:57:27 +1000

    wip spritesheet coords

--- R/sprite-utils.R
+++ R/sprite-utils.R
@@ -28,7 +28,7 @@ if (FALSE) {
   w <- 100
   h <- 100
   w_sprite <- 27
-  h_sprite <- 24
+  h_sprite <- 25
   
   plot.new()
   
@@ -36,15 +36,19 @@ if (FALSE) {
     nr <- nr_new(w, h, 'grey90')
     nr_blit(nr, 1, 1, ss)
     nr_rect(nr, x-1, y-1, w_sprite+2, h_sprite+2, fill = NA, colour = '#00000020')
-    grid.newpage(); grid.raster(nr, interpolate = FALSE)
+    grid.newpage(); 
+    grid.rect(x = 0, y = 0, width = 0.1, height = 0.1, just = c(0, 0), gp = gpar(fill = 'blue'))
+    grid.raster(nr, interpolate = FALSE)
     
     loc <- locator(1)  
     loc$x <- loc$x - 0.5
     loc$y <- loc$y - 0.5
     
-    print(unlist(loc))
+    # print(unlist(loc))
     
-    if (loc$x > 0) {
+    if (loc$x < -0.4 && loc$y < -0.4) {
+      cat(sprintf("c(%i, %i, %i, %i)\n", x, y, w_sprite, h_sprite))
+    } else if (loc$x > 0) {
       if (loc$x > abs(loc$y)) {
         x <- x + 1
       } else if (loc$y > 0) {
