fa241ebb369b8c3c88dee20a392fe811940dbe63
mike <mikefc@coolbutuseless.com>
2024-05-30 10:28:20 +1000

    working on an easy sprite editor

---
+++ R/sprite-utils.R
@@ -0,0 +1,66 @@
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Helper for finding coordinates in a spritesheet
+#' 
+#' @param ss sprite sheet (nativeRaster)
+#' 
+#' @noRd
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sprite_finder <- function(ss, w, h, w_sprite, h_sprite, x = 1, y = 1) {
+  
+  
+  
+}
+
+if (FALSE) {
+  
+  
+  source <- "working/dino-spritesheet.png"
+  ss <- read_png(source, transparent = list(-1594494))
+  if (FALSE) {
+    grid.newpage(); grid.raster(ss)
+  }
+  
+  x <- 7
+  y <- 5
+  
+  w <- 100
+  h <- 100
+  w_sprite <- 27
+  h_sprite <- 24
+  
+  plot.new()
+  
+  while (TRUE) {
+    nr <- nr_new(w, h, 'grey90')
+    nr_blit(nr, 1, 1, ss)
+    nr_rect(nr, x-1, y-1, w_sprite+2, h_sprite+2, fill = NA, colour = '#00000020')
+    grid.newpage(); grid.raster(nr, interpolate = FALSE)
+    
+    loc <- locator(1)  
+    loc$x <- loc$x - 0.5
+    loc$y <- loc$y - 0.5
+    
+    print(unlist(loc))
+    
+    if (loc$x > 0) {
+      if (loc$x > abs(loc$y)) {
+        x <- x + 1
+      } else if (loc$y > 0) {
+        y <- y + 1
+      } else if (loc$y < 0) {
+        y <- y - 1
+      }
+    } else {
+      if (abs(loc$x) > abs(loc$y)) {
+        x <- x - 1
+      } else if (loc$y > 0) {
+        y <- y + 1
+      } else if (loc$y < 0) {
+        y <- y - 1
+      }
+    }
+  }
+  
+}
