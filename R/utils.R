b66f799aa1283c788c7768a9b954efba1604ff27
mike <mikefc@coolbutuseless.com>
2024-05-31 19:37:51 +1000

    add plot.nativeRaster() [no ci]

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,21 @@
 is_nativeraster <- function(x) {
   inherits(x, 'nativeRaster') && attr(x, 'channels', exact = TRUE) == 4
 }
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Plot a native raster as an image
+#' 
+#' @param x native raster
+#' @param y any argument here will cause the page to be called using a call
+#'        to \code{grid::grid.newpage()}
+#' @param ... other arguments passed to \code{grid::grid.raster()}
+#'
+#' @return None.
+#' @import grid
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+plot.nativeRaster <- function(x, y, ...) {
+  if (!missing(y)) grid::grid.newpage()
+  grid::grid.raster(x, interpolate = FALSE, ...)
+}
