39ac8c511822479b9887d31b674140f9e28d3840
mike <mikefc@coolbutuseless.com>
2026-04-30 11:44:17 +1000

    refactoring tri drawing

--- R/nr-tri.R
+++ R/nr-tri.R
@@ -59,24 +59,104 @@ nr_wireframe_coords <- function(nr, coords, col, draw_all = FALSE, format = 'aut
 
 if (FALSE) {
   library(grid)
+  library(insitu)
   
   w   <- 200
   h   <- 200
   fac <- 5
   
   obj <- rgl::icosahedron3d()
+  ntri <- ncol(obj$it)
+  set.seed(2)
+  cols <- viridisLite::inferno(ntri) |> sample() # colors for triangles
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # flatten mesh data to pure vertex coordinate data
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   off <- w / 2
-  coords <- with(obj, vb[, obj$it])
-  coords <- (coords * 60) + off
+  vertices <- with(obj, vb[, obj$it])
+  vertices <- (vertices * 40) + off
+  
+  pvertices <- vertices
+  pvertices[] <- vertices
   
   nr <- nr_new(w, h)
-  nr_tri_coords(nr, coords, 'red')
+  nr_tri_coords(nr, pvertices, cols)
   grid.raster(nr, interpolate = FALSE)
   
+}
+
+
+
+if (FALSE) {
+  library(grid)
+  library(insitu)
+  library(naratigr)
+  
+  w   <- 200
+  h   <- 200
+  fac <- 5
+  off <- w / 2
+  
+  # x11(width = w * fac / dpi, height = h * fac / dpi, antialias = 'none', type = 'dbcairo')
+  # dev.control(displaylist = 'inhibit')
+  window <- naratigr::tigr_open(w, h)
+  
+  
+  # obj <- readobj::read.obj("working/obj/bunny.obj")
+  # vertices <- obj$shapes[[1]]$positions[, obj$shapes[[1]]$indices]
+  # vertices <- (vertices * 800) + off
+  # ntri <- ncol(vertices) / 3
+  
+  # obj <- rgl::readOBJ("working/obj/newell_teaset/teapot.obj")
+  obj <- rgl::readOBJ('working/obj/bunny2.obj')
+  # obj <- rgl::addNormals(obj)
+  # obj <- rgl::icosahedron3d()
+  
+  ntri <- ncol(obj$it)
+  vertices <- with(obj, vb[, obj$it])
+  vertices <- (vertices * 100) + off
+  
+  cols <- viridisLite::inferno(ntri) |> sample() # colors for triangles
+  pvertices <- vertices
+  pvertices[] <- vertices
+  
+  nr <- nr_new(w, h, fill = 'blue')
+  tigr_update(window, nr)
+
+  frame <- 1
+  nframes <- 600
+  start <- Sys.time()
+  for (frame in seq_len(nframes)) {
+
+    nr_fill(nr, 'white')
+    nr_tri_coords(nr, pvertices, cols)
+    # dev.hold()
+    # grid.raster(nr, interpolate = FALSE)
+    # dev.flush()
+    tigr_update(window, nr)
+    
+    # The transformation function is dependent upon the frame number
+    # in order to get a nice sinusoidal rotation effect
+    frac <- (frame - 1)/nframes
+    tf <- tf3_new() |> 
+      tf3_add_translate(x = -off, y = -off, z = -off) |>
+      tf3_add_rotate_x(2   * sin(2 * pi * frac + pi/4)) |>
+      tf3_add_rotate_y(1   * sin(2 * pi * frac)) |>
+      tf3_add_rotate_z(1.5 * sin(2 * pi * frac + pi/3)) |>
+      # tf3_add_rotate_y(7 * nframes/300 * cos(frac * 2 + pi/4)) |>
+      # tf3_add_rotate_z(3 * nframes/300 * sin(frac * 8 + pi/2)) |>
+      tf3_add_translate(x = off, y = off, z = off)
+    
+    # Start with the raw vertex data and transform it, ready for the next frame
+    br_copy(pvertices, vertices)
+    pvertices |>
+      br_mat_transpose() |>
+      tf3_apply(tf) |>
+      br_mat_transpose()
+    
+  }
+  print(Sys.time() - start)
   
 }
 
--- src/nr-draw-tri.c
+++ src/nr-draw-tri.c
@@ -19,6 +19,29 @@
 #define FMT_WIDE 1
 #define FMT_LONG 2
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// is (cx, cy) to the left of the line from (ax, ay) to (bx, by)?
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// double left_of(double ax, double ay, double bx, double by, double cx, double cy) {
+//   return ((by - ay) * (cx - ax) - (bx - ax) * (cy - ay)) >= 0;
+// }
+
+double det(double ax, double ay, double bx, double by, double cx, double cy) {
+  return ((by - ay) * (cx - ax) - (bx - ax) * (cy - ay));
+}
+
+
+double min3(double a, double b, double c) {
+  double temp = (a < b)    ? a : b;
+  return (c < temp) ? c : temp;
+}
+
+
+double max3(double a, double b, double c) {
+  double temp = (a > b) ? a : b;
+  return (c > temp) ? c : temp;
+}
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 
@@ -30,18 +53,51 @@ void draw_tri(uint32_t *nr, int nr_width, int nr_height,
               int color, 
               draw_mode_t draw_mode) {
   
-  nr_line(nr, nr_width, nr_height, x0, y0, x1, y1, color, 1, draw_mode);
-  nr_line(nr, nr_width, nr_height, x1, y1, x2, y2, color, 1, draw_mode);
-  nr_line(nr, nr_width, nr_height, x2, y2, x0, y0, color, 1, draw_mode);
+  // // Do nothing if triangle not oriented correctly
+  bool tri_ok = det(x0, y0, x1, y1, x2, y2) >= 0;
+  if (!tri_ok) return;
+
+  // if (!tri_ok) {
+  //   // swap 2 verts to make it ok
+    // double tmp;
+    // tmp = x0; x0 = x1; x1 = tmp;
+    // tmp = y0; y0 = y1; y1 = tmp;
+  // }
   
-  nr_point(nr, nr_width, nr_height, x0, y0, color, draw_mode);
-  nr_point(nr, nr_width, nr_height, x1, y1, color, draw_mode);
-  nr_point(nr, nr_width, nr_height, x2, y2, color, draw_mode);
+  
+  double xmin = min3(x0, x1, x2);
+  double xmax = max3(x0, x1, x2);
+  double ymin = min3(y0, y1, y2);
+  double ymax = max3(y0, y1, y2);
+  
+  xmin = xmin < 0 ? 0 : xmin;
+  xmax = xmax > nr_width ? nr_width : xmax;
+  ymin = ymin < 0 ? 0 : ymin;
+  ymax = ymax > nr_height ? nr_height : ymax;
+  
+  for (int x = xmin; x < xmax; x++) {
+    for (int y = ymin; y < ymax; y++) {
+      if (det(x0, y0, x1, y1, x, y) >= 0 &&
+          det(x1, y1, x2, y2, x, y) >= 0 &&
+          det(x2, y2, x0, y0, x, y) >= 0) {
+        nr_point(nr, nr_width, nr_height, x, y, color, draw_mode);
+      } 
+    }
+  }
+  
+  
+  // nr_line(nr, nr_width, nr_height, x0, y0, x1, y1, 0xFF000000, 1, draw_mode);
+  // nr_line(nr, nr_width, nr_height, x1, y1, x2, y2, 0xFF000000, 1, draw_mode);
+  // nr_line(nr, nr_width, nr_height, x2, y2, x0, y0, 0xFF000000, 1, draw_mode);
+  // 
+  // nr_point(nr, nr_width, nr_height, x0, y0, color, draw_mode);
+  // nr_point(nr, nr_width, nr_height, x1, y1, color, draw_mode);
+  // nr_point(nr, nr_width, nr_height, x2, y2, color, draw_mode);
 }
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// 
+// Integer coordinates only
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SEXP nr_tri_coords_(SEXP nr_, SEXP coords_, SEXP color_, SEXP draw_all_, SEXP format_) {
   
@@ -72,7 +128,7 @@ SEXP nr_tri_coords_(SEXP nr_, SEXP coords_, SEXP color_, SEXP draw_all_, SEXP fo
   bool free_color = false;
   uint32_t *color = multi_rcolors_to_ints(color_, n_tris, &free_color);
   
-  Rprintf("%i / %i\n", n_coords, n_tris);
+  // Rprintf("%i / %i\n", n_coords, n_tris);
   draw_mode_t draw_mode = 1; // respect alpha
   int col_idx = 0;
   for (int i = 0; i < n_coords * stride; i += 3 * stride) {
